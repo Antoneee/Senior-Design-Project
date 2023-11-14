2788457945a99cf8ae11a7f6e45525521696d0a0
Anthony Ngo <anthonyhtngo@gmail.com>
2023-11-13 21:01:54 -0600

    Add video call functionality to backend

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,16 @@ const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
 const countryRoutes = require("./routes/countryRoutes");
 const languageRoutes = require("./routes/languageRoutes");
+const http = require("http");
 
 const app = express();
+const server = http.createServer(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 require("dotenv").config();
 
 app.use(cors());
@@ -16,6 +24,26 @@ app.use(
     extended: true,
   })
 );
+
+io.on("connection", (socket) => {
+  socket.emit("me", socket.id);
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("callEnded");
+  });
+
+  socket.on("callUser", (data) => {
+    io.to(data.userToCall).emit("callUser", {
+      signal: data.signalData,
+      from: data.from,
+      name: data.name,
+    });
+  });
+
+  socket.on("answerCall", (data) => {
+    io.to(data.to).emit("callAccepted", data.signal);
+  });
+});
 
 app.use(authRoutes);
 app.use(countryRoutes);
